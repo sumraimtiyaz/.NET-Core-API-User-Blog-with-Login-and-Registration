b6d6d20a0d5e5f4464a22ff81b92438f5e226ad2
sumraimtiyaz <sumra_imtiyaz@yahoo.com>
2025-03-24 10:05:28 +0530

    MySQL Database Script added for the reference to created database object

---
+++ MySQL_Database_Script.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE UserBlogDB;
+
+USE UserBlogDB;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    role ENUM('User', 'Admin','Other') NOT NULL DEFAULT 'User'
+);
+
+CREATE TABLE articles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
